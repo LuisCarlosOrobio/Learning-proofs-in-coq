6cdc773fe4f006cd4cd124744fd5be290e2b267e
LuisCarlosOrobio <140061957+LuisCarlosOrobio@users.noreply.github.com>
2024-06-19 02:07:27 -0600

    Update proofs.v

--- proofs.v
+++ proofs.v
@@ -1,7 +1,20 @@
+Require Import Coq.Arith.Arith.
+
 Theorem add_assoc : forall a b c : nat, a + (b + c) = (a + b) + c.
 Proof.
 intros a b c.
 induction a as [| a' IHa'].
 - reflexivity.
 - simpl. rewrite -> IHa'. reflexivity.
+Qed.
+
+Theorem mult_comm : forall a b : nat, a * b = b * a.
+Proof.
+  intros a b.
+  induction a as [| a' IHa'].
+  - simpl. rewrite Nat.mul_0_r. reflexivity.
+  - simpl. rewrite IHa'.
+    rewrite Nat.mul_succ_r.
+    rewrite Nat.add_comm.
+    reflexivity.
 Qed.
